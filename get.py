1311be9ea86d74952f69ad8cc8686c10d27b8f95
Jack W <jackweber943@gmail.com>
2023-08-08 12:01:29 -0400

    Created get.py Function
    get.py takes an input from the user and checks to see if the 2 numbers are actually numbers.

--- get.py
+++ get.py
@@ -1,2 +1,13 @@
 def get():
-    print("Hello World!")
+    while True:
+        num1 = input("Please provide a number: ")
+        num2 = input("Please provide another number: ")
+        op = input("Please provide a mathamatical operator: ")
+        try:
+            int(num1)
+            int(num2)
+            break
+        except ValueError:
+            continue
+
+get()
