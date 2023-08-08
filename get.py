b1453dd66bfd8f1a3eb27d25ae716d9c102997e1
Jack W <jackweber943@gmail.com>
2023-08-08 14:21:25 -0400

    Operator Check
    The code now checks to see if the user inputed a correct operator. If not, the while True loop continues to run.

--- get.py
+++ get.py
@@ -1,14 +1,19 @@
+operators = ["+", "-", "*", "/"]
 def get():
     while True:
         num1 = input("Please provide a number: ")
         num2 = input("Please provide another number: ")
         op = input("Please provide a mathamatical operator: ")
+
         try:
             int(num1)
             int(num2)
-            break
         except ValueError:
             print("PLEASE ENTER VALID NUMBERS")
+            continue      
+        if op in operators:
+            break
+        else: 
+            print("Invalid operator, please submit a valid one")
             continue
-
 get()
