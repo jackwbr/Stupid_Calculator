804cf926bef5b9455cb80e0996c1589715b5e487
Jack W <jackweber943@gmail.com>
2023-08-08 14:35:20 -0400

    stupid_math updates
    stupid math looks for what to do.

--- stupid_math.py
+++ stupid_math.py
@@ -1,2 +1,14 @@
 from get import get 
+
+
+def whatToDo(num1, num2, op):
+    if op == "+":
+        wtd = "add"
+    if op == "-":
+        wtd = "subtract"
+    if op == "*":
+        wtd = "multiply"
+    if op == "/":
+        wtd = "divide"
 get()
+whatToDo()
